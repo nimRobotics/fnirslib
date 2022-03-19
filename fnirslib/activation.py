d67308f986a972ce642b56ac28c038609fb26972
nimrobotics <aakash.iitt@gmail.com>
2022-03-18 23:16:53 -0500

    todo: activations

---
+++ fnirslib/activation.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def getMeanActivation():
+    """
+    Get mean activation for each region
+    :return: mean activation for each region
+    """
+    raise NotImplementedError
+
+def getPeakActivation():
+    """
+    Get peak activation for each region
+    :return: peak activation for each region
+    """
+    raise NotImplementedError
